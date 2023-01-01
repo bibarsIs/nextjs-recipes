ec17a113719584b85a6d768b48d131abd558254e
bibars <bibars2000@gmail.com>
2023-01-01 18:45:39 +0600

    updated seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -10,52 +10,77 @@ async function main() {
             { title: 'ketchup' },
             { title: 'flour' },
             { title: 'sugar' },
+            { title: 'peanut butter' },
+            { title: 'jam' },
+            { title: 'bread' },
         ]
     })
 
     await prisma.recipe.create({
-        data: {
-            title: 'pancakes',
-            instructions: 'mix ingredients and fry the batter',
-            ingredients: {
-                connect: [
-                    { title: 'egg' },
-                    { title: 'milk' },
-                    { title: 'flour' },
-                    { title: 'sugar' },
-                    { title: 'peanut butter' },
-                    { title: 'jam' },
-                    { title: 'bread' },
-                ]
+            data: {
+                title: 'omelette',
+                instructions: 'make an omelette and drizzle ketchup',
+                ingredients: {
+                    create: [
+                        {
+                            quantity: 4,
+                            ingredient: { connect: { title: 'egg' } }
+                        },
+                        {
+                            ingredient: { connect: { title: 'ketchup' } }
+                        },
+                    ]
+                }
             }
         }
-    })
+    )
     await prisma.recipe.create({
-        data: {
-            title: 'omelette',
-            instructions: 'make an omelette and drizzle ketchup',
-            ingredients: {
-                connect: [
-                    { title: 'egg' },
-                    { title: 'ketchup' },
-                ]
+            data: {
+                title: 'pancakes',
+                instructions: 'mix wet ingredients and fry',
+                ingredients: {
+                    create: [
+                        {
+                            quantity: 4,
+                            ingredient: { connect: { title: 'egg' } }
+                        },
+                        {
+                            ingredient: { connect: { title: 'flour' } }
+                        },
+                        {
+                            ingredient: { connect: { title: 'sugar' } }
+                        },
+                        {
+                            ingredient: { connect: { title: 'milk' } }
+                        },
+                    ]
+                }
             }
         }
-    })
+    )
     await prisma.recipe.create({
-        data: {
-            title: 'peanut butter and jam sandwich',
-            instructions: 'spread wet ingredients on bread',
-            ingredients: {
-                connect: [
-                    { title: 'bread' },
-                    { title: 'jam' },
-                    { title: 'peanut butter' },
-                ]
+            data: {
+                title: 'PBJ sandwich',
+                instructions: 'assemble the sandwich',
+                ingredients: {
+                    create: [
+                        {
+                            ingredient: { connect: { title: 'peanut butter' } }
+                        },
+                        {
+                            ingredient: { connect: { title: 'jam' } }
+                        },
+                        {
+                            quantity: 2,
+                            ingredient: { connect: { title: 'bread' } }
+                        },
+                    ]
+                }
             }
         }
-    })
+    )
 
+    //  end of main()
 }
 
 main()
