e565f1b58fbec000b125aa38dd32ee8fb8a8bd76
bibars <bibars2000@gmail.com>
2023-01-11 20:45:17 +0600

    added middleware.ts

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,7 +10,11 @@ export default function Navbar() {
             <li className='pr-8'><Link href='/'><span
                 className={ `border-2 border-black p-3 bg-teal-400 ${ pathname === '/' ? 'underline' : '' }` }>Home</span></Link>
             </li>
-            <li><Link href='/recipes' className={ `${ pathname === '/recipes' ? 'underline' : '' }` }>All recipes</Link>
+            <li>
+                <Link href='/recipes' className={ `${ pathname === '/recipes' ? 'underline' : '' }` }>All recipes</Link>
+            </li>
+            <li>
+                <Link href='/favorites' className={ `${ pathname === '/favorites' ? 'underline' : '' }` }>Favorites</Link>
             </li>
             {/*
             <li>Register</li>
